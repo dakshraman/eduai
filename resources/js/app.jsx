8cdbb2023bedad3bee187ff8b3b385b2e9771d08
dakshraman <ramandaksh6161@gmail.com>
2026-09-05 08:36:41 +0000

    Fix CSS loading: add app.css as separate Vite entry

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { createInertiaApp } from '@inertiajs/react';
 import { createRoot } from 'react-dom/client';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import '../css/app.css';
 
 createInertiaApp({
     resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>EduAI</title>
-    @vite(['resources/js/app.jsx'])
+    @vite(['resources/css/app.css', 'resources/js/app.jsx'])
     @inertiaHead
 </head>
 <body>
